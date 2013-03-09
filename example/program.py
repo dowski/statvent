d6c170505ee2767173a07a917c7afc9864509882
Christian Wyglendowski <christian@dowski.com>
2013-03-09 00:34:51 -0500

    Added an example

---
+++ example/program.py
@@ -0,0 +1,24 @@
+"""Example program to demonstrate pipestatic usage."""
+import os
+import random
+import time
+
+from pipestatic import stats
+
+
+def main():
+    pid = os.getpid()
+    print "stats being recorded to /tmp/stats-pipe/%d.stats" % pid
+    print "Try 'cat /tmp/stats-pipe/%d.stats'" % pid
+    stats.start_recorder()
+    i = 0
+    while True:
+        i += 1
+        stats.incr('foo.bar', 1)
+        stats.set('foo :: now', time.time())
+        stats.record('foo->baz->{0}', random.uniform(0.0, 5.0))
+        if i % 50 == 0:
+            print "tick ..."
+        time.sleep(.1)
+if __name__ == '__main__':
+    main()
