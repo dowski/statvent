f4f4a974cbe1d895456a7b85a9658c57eff3af87
erin <edahlgren@bu.mp>
2013-03-08 16:27:24 -0800

    add a record method that lazily spawns a aggregate calculation before publishing

--- pipestatic/stats.py
+++ pipestatic/stats.py
@@ -27,11 +27,12 @@ import threading
 import time
 import traceback
 import urlparse
+import math
 
 import simplejson
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 STATS_ROOT = '/tmp/stats-pipe'
@@ -52,6 +53,13 @@ def incr(name, value=1):
 
     """
     _stats[name] += value
+
+def record(name, value):
+    """Record the given name by value,
+       to be pre-processed by the StatsRecorder's
+       calculator before being set
+    """
+    _deque[name].append(value)
 
 def get_all():
     """Return a dictionary of the recorded stats."""
@@ -113,14 +121,44 @@ def http_stat_publisher(ip='', port=7828, path='/stats'):
 # ============
 
 _stats = defaultdict(int)
+_deque = None
 _recorder = None
+
+def splice(name, label):
+    i = name.index('[')
+    basename = '.'.join([name[:i], label])
+    tags = name[i:]
+    return basename + tags
+
+def basic_percentiles(name, vals):
+    n_vals = len(vals)
+    PERCENTILES = [(50, "median"), (95, "95th"), (99, "99th"), (100, "100th")]
+
+    for n,label in PERCENTILES:
+        index = int(math.floor(n_vals * (n * 0.01))) - 1
+        if index < 0:
+            index = 0
+        yield (splice(name, label), vals[index] if vals else 0.0)
+    if n_vals:
+        yield (splice(name, "mean"), sum(vals) / n_vals)
+
 
 class _StatRecorder(threading.Thread):
 
-    def __init__(self):
+    def __init__(self, calculator=basic_percentiles, deque_size=100):
         super(_StatRecorder, self).__init__()
         default_filename = "%s.stats" % (os.getpid())
         self.statpath = os.path.join(STATS_ROOT, default_filename)
+        self.calculator = calculator
+
+        global _deque
+        _deque = defaultdict(lambda: deque(list(), deque_size))
+
+    def set_deque(self):
+        for name, vals in _deque.iteritems():
+            vals = sorted(vals)
+            for (metric, val) in self.calculator(name, vals):
+                set(metric, val)
 
     def run(self):
 
@@ -134,6 +172,7 @@ class _StatRecorder(threading.Thread):
         while True:
             os.mkfifo(self.statpath)
             f = open(self.statpath, 'w')
+            self.set_deque()
             for name, value in get_all().iteritems():
                 if isinstance(value, float):
                     f.write('%s: %f\n' % (name, value))
