e59f87b79d07c76001f3919c76232dbacfb874d7
Christian Wyglendowski <christian@dowski.com>
2013-03-15 19:11:59 -0400

    Update to the example program
    No functional changes. Just changed an import and added a few comments.

--- example/program.py
+++ example/program.py
@@ -3,22 +3,27 @@ import os
 import random
 import time
 
-from statvent import stats
+import statvent
 
 
 def main():
     pid = os.getpid()
     print "stats being recorded to /tmp/stats-pipe/%d.stats" % pid
     print "Try 'cat /tmp/stats-pipe/%d.stats'" % pid
-    stats.start_recorder()
+
+    # Start the thread that will write out stats to the pipe.
+    statvent.start_recorder()
+
+    # Track a bunch of boring demo stats.
     i = 0
     while True:
         i += 1
-        stats.incr('foo.bar', 1)
-        stats.set('foo :: now', time.time())
-        stats.record('foo->baz->{0}', random.uniform(0.0, 5.0))
+        statvent.incr('foo.bar', 1)
+        statvent.set('foo :: now', time.time())
+        statvent.record('foo->baz->{0}', random.uniform(0.0, 5.0))
         if i % 50 == 0:
             print "tick ..."
         time.sleep(.1)
+
 if __name__ == '__main__':
     main()
