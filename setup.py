d57c16fb403f424b130d970b49c645cd9bdec222
Christian Wyglendowski <christian@dowski.com>
2013-03-16 00:34:42 -0400

    s/module/library in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     author="Christian Wyglendowski",
     author_email="christian@dowski.com",
-    description="A simple module that writes stats about your program to a named pipe.",
+    description="A simple library that writes stats about your program to a named pipe.",
     license="BSD",
     keywords="monitoring stats statistics counters pipe",
     url="https://github.com/dowski/statvent/",
