60c5eac5a4c4041d787410bd758ac088c6681726
Christian Wyglendowski <christian@dowski.com>
2013-03-27 14:45:45 -0400

    Release with fixed package
    The README wasn't being distributed before which broke setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="statvent",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     author="Christian Wyglendowski",
     author_email="christian@dowski.com",
